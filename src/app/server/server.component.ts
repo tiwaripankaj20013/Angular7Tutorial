adbfd8030518dd4fbc69a7d1a8a774e2f7760ff8
tiwaripankaj20013 <34410355+tiwaripankaj20013@users.noreply.github.com>
2019-03-19 19:16:59 +0530

    angular 7 tutorial
    Angular 7 from tutorial from udemy. completed 20 tutorial

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    // selector: '[app-server]',
+   // selector: '[.app-server]',
+    templateUrl:'./server.component.html'
+})
+
+export class ServerComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
+ 
