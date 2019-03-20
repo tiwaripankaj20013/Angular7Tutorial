e86cca1e2b7b1626c74734f85d15e4ee2f3c9161
pankajaltudo <47175119+pankajaltudo@users.noreply.github.com>
2019-03-20 18:12:01 +0530

    completed tutorial 21

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
-    selector: 'app-server',
+    //selector: '.app-server', class selector
     // selector: '[app-server]',
    // selector: '[.app-server]',
+    selector: 'app-server',
     templateUrl:'./server.component.html'
 })
 
 export class ServerComponent implements OnInit {
+   serverId: number=10;
+   serverStatus: string='offline';
     constructor() { }
-
     ngOnInit() { }
 }
  
